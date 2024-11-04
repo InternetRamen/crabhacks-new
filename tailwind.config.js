6cb39fdc46c7ca96effa0d402e444c1e5cb081fe
Jaden Hou <jadenhou19@gmail.com>
2024-11-04 11:21:40 -0500

    glow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
     content: ["*.{html,js}"],
     theme: {
-        extend: {
-            dropShadow: {
-                glow: [
-                    "0 0px 20px rgba(255,255, 255, 0.35)",
-                    "0 0px 65px rgba(255, 255,255, 0.2)",
-                ],
-            },
-        },
+        extend: {},
     },
     plugins: [require("daisyui")],
     daisyui: {
